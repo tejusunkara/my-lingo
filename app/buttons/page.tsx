00b05a1ac7bc093595a9ac91b7fde0c8854cd49f
Venkata <jnanatejaswini@gmail.com>
2024-06-01 08:33:46 -0400

    create nextjs project

---
+++ app/buttons/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+const ButtonsPage = () => {
+  return (
+    <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="primary">Secondary</Button>
+      <Button variant="primaryOutline">Secondary Outline</Button>
+    </div>
+  );
+};
+
+export default ButtonsPage;
