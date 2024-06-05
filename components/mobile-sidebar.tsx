cedb0c2860bd3a93223daf5d9c281b4f660284ae
Venkata <jnanatejaswini@gmail.com>
2024-06-04 20:50:51 -0400

    create skeleton main layout

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,16 @@
+import { Menu } from "lucide-react";
+import { Sidebar } from "@/components/sidebar";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-white" />
+      </SheetTrigger>
+      <SheetContent className="p-0 z-[100]" side="left">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  )
+}
