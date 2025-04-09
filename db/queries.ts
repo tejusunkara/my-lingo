b43613289e1a0019ce352d5286f5fad0fd4eaf27
Venkata Sunkara <jnanatejaswini@gmail.com>
2025-04-09 18:42:11 -0400

    update seed script and create get units query

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { courses, userProgress } from "./schema";
+import { courses, userProgress, units, } from "./schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -14,12 +14,90 @@ export const getUserProgress = cache(async () => {
   const data = await db.query.userProgress.findFirst({
     where: eq(userProgress.userId, userId),
     with: {
+
       activeCourse: true,
     },
   });
 
   return data;
-})
+});
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+
+  if(!userProgress?.activeCourseId) {
+    return null;
+  }
+
+  // TODO: update to use Drizzle SQL query builder
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: true,
+            },
+          }
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allChallengesCompleted = lesson.challenges.every(
+        (challenge) => {
+          return challenge.challengeProgress
+            && challenge.challengeProgress.length > 0
+            && challenge.challengeProgress.every((progress) => progress.completed)
+        });
+
+      return {
+        ...lesson,
+        completed: allChallengesCompleted,
+      };
+    });
+
+    return {
+      ...unit,
+      lessons: lessonsWithCompletedStatus,
+    };
+  });
+
+  // const normalizedData = data.map((unit) => {
+  //   const unitLessons = data.filter((row) => row.lessons?.unitId === unit.id);
+    
+  //   const lessons = unitLessons.map((row) => {
+  //     if (!row.lessons) return null;
+
+  //     const lessonChallenges = data.filter(
+  //       r => r.challenges?.lessonId === row.lessons.id
+  //     );
+
+  //     const isCompleted = lessonChallenges.length > 0 && 
+  //       lessonChallenges.every(r => r.challengeProgress?.completed);
+
+  //     return {
+  //       ...row.lessons,
+  //       completed: isCompleted,
+  //     };
+  //   }).filter((l): l is NonNullable<typeof l> => l !== null);
+
+  //   const isUnitCompleted = lessons.length > 0 && 
+  //     lessons.every(lesson => lesson.completed);
+
+  //   return {
+  //     ...unit,
+  //     completed: isUnitCompleted,
+  //     lessons,
+  //     unitLessons,
+  //   };
+  // });
+
+  return normalizedData;
+});
 
 export const getCourses = cache(async () => { // will cache response for any calls made in any other component
   const data = await db.query.courses.findMany();
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,13 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+
+
 
     await db.insert(schema.courses).values([
       {
@@ -42,6 +49,70 @@ const main = async () => {
       },
     ]);
 
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        title: "Unit 1",
+        courseId: 1,
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        title: "Nouns",
+        unitId: 1,
+        order: 1,
+      },
+      // {
+      //   id: 2,
+      //   title: "Adjectives",
+      //   unitId: 1,
+      //   order: 2,
+      // },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: "SELECT",
+        question: 'Which one of these means "the man?',
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1,
+        option: "el hombre",
+        text: "el hombre",
+        isCorrect: true,
+        imageSrc: "/man.svg",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1,
+        option: "la mujer",
+        text: "la mujer",
+        isCorrect: false,
+        imageSrc: "/woman.svg",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1,
+        option: "el niño",
+        text: "el niño",
+        isCorrect: false,
+        imageSrc: "/boy.svg",
+        audioSrc: "/es_boy.mp3",
+      },
+    ]);
     console.log("Seeding finished");
   } catch (error) {
     console.error(error);
