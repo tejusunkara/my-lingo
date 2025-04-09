879ba7c68cce8cdbb2444968fa8afeb0c7b7b2e2
Venkata Sunkara <jnanatejaswini@gmail.com>
2025-04-09 13:53:30 -0400

    add challenge progress and options to schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { integer, pgEnum, pgTable, serial, text } from "drizzle-orm/pg-core"; 
+import { integer, pgEnum, pgTable, serial, text, boolean } from "drizzle-orm/pg-core"; 
 
 // courses data table
 export const courses = pgTable("courses", {
@@ -10,6 +10,7 @@ export const courses = pgTable("courses", {
 
 export const coursesRelations = relations(courses, ({ many }) => ({ // many-to-many relation for userProgress and courses
   userProgress: many(userProgress),
+  units: many(units),
 }));
 export const units = pgTable("units", {
   id: serial("id").primaryKey(),
@@ -56,6 +57,39 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
   lesson: one(lessons, {
     fields: [challenges.lessonId],
     references: [lessons.id],
+  }),
+  challengeOptions: many(challengeOptions),
+  challengeProgress: many(challengeProgress),
+}));
+
+export const challengeOptions = pgTable("challenge_options", {
+  id: serial("id").primaryKey(),
+  challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
+  option: text("option").notNull(),
+  text: text("text").notNull(),
+  isCorrect: boolean("is_correct").notNull(),
+  imageSrc: text("image_src"),
+  audioSrc: text("audio_src"),
+});
+
+export const challengeOptionsRelations = relations(challengeOptions, ({ one }) => ({
+  challenge: one(challenges, {
+    fields: [challengeOptions.challengeId],
+    references: [challenges.id],
+  }),
+}));
+
+export const challengeProgress = pgTable("challenge_progress", {
+  id: serial("id").primaryKey(),
+  challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
+  userId: text("user_id").notNull(),
+  completed: boolean("completed").notNull().default(false),
+});
+
+export const challengeProgressRelations = relations(challengeProgress, ({ one }) => ({
+  challenge: one(challenges, {
+    fields: [challengeProgress.challengeId],
+    references: [challenges.id],
   }),
 }));
 
